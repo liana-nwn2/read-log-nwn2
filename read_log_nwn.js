d54e04f3d09f5be60c660287a6f0e60bc9d46f26
liana-nwn2 <74141168+liana-nwn2@users.noreply.github.com>
2020-11-22 22:18:01 +0100

    Add files via upload
    Prise en compte des langues

--- read_log_nwn.js
+++ read_log_nwn.js
@@ -1,15 +1,33 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
+   /*
+   // Détection de la langue
+   if (/^fr\b/.test(navigator.language)) {
+
+   } else {
+
+   }
+    */
+
+
    const folderPicker = document.getElementById("folderPicker")
    folderPicker.addEventListener("change", function (event) {
 
       const warningInfo = document.querySelector('.warning_info')
-      let listFiles = initFileList(this.files)
+      let allFiles = initFileList(this.files)
+      let listFiles = allFiles.chatLog
+      let languesFiles = allFiles.languesLog
+
       warningInfo.style.display = 'none'
 
       if (listFiles.length > 0) {
+         // Récupère les langues
+         openFiles(languesFiles, 'langues', '')
+
          setupFileListing(listFiles)
          sortFiles(document.getElementById('fileSort').checked)
+
+
       } else {
          warningInfo.style.display = 'flex'
       }
@@ -26,20 +44,26 @@ document.addEventListener("DOMContentLoaded", function (e) {
 // ---------------------------------------------------------
 const initFileList = (listFiles) => {
 
-   let listFilesDef = []
+   let logFilesDef = []
+   let traducFilesDef = []
 
    for (let thisFile of listFiles) {
 
       let isFolder = thisFile.webkitRelativePath.match(/[/]/g).length !== 1
       let isChatLog = thisFile.name.match('Chatlog')
+      let isCombatLog = thisFile.name.match('Combatlog')
 
-      if (!isFolder && isChatLog) {
-         listFilesDef.push(thisFile)
+      if (!isFolder) {
+         if (isChatLog)
+            logFilesDef.push(thisFile)
+         if (isCombatLog) {
+            traducFilesDef.push(thisFile)
+         }
       }
 
    }
 
-   return listFilesDef
+   return {chatLog: logFilesDef, languesLog: traducFilesDef}
 
 }
 // ---------------------------------------------------------
@@ -264,7 +288,7 @@ const setDynamicsEvents = (listingFilesUl, fileListe) => {
 
       thisLi.classList.add('selected')
       updateNavig(thisLi, listeLi.length, document.querySelectorAll('.file_prev, .file_next, .file_name, .file_num'))
-      openFiles([fileListe[thisLi.dataset.index]], false, '')
+      openFiles([fileListe[thisLi.dataset.index]], 'process')
 
    }
 
@@ -310,9 +334,9 @@ const setDynamicsEvents = (listingFilesUl, fileListe) => {
 
          if (checkByDate) {
             newFileListe = updateListeFilesByDate(fileListe)
-            openFiles(newFileListe, true, searchOptions)
+            openFiles(newFileListe, 'search', searchOptions)
          } else {
-            openFiles(fileListe, true, searchOptions)
+            openFiles(fileListe, 'search', searchOptions)
          }
 
          document.querySelector('.back_to_liste').classList.add('show')
@@ -527,11 +551,12 @@ const resizeResult = () => {
 // ---------------------------------------------------------
 //    OUVERTURE D'UN LOG
 // ---------------------------------------------------------
-const openFiles = (listeFiles, search, searchOptions) => {
+const openFiles = (listeFiles, mode, searchOptions) => {
 
    const body = document.body
 
    let fileindex = 1
+   let languesLines = []
    let searchResult = []
    let newFileListe = []
    let allPos = []
@@ -551,62 +576,93 @@ const openFiles = (listeFiles, search, searchOptions) => {
 
       Promise.all([filePromise]).then(fileContents => {
 
-         const matchLineSearch = /([\[][0-9]{2}:[0-9]{2}] .*?: \[.*?\] [^\[]+)/g
-         const thisLogFormatted = fileContents.join('').match(new RegExp(matchLineSearch))
-         let result
+         let matchLineSearch, thisLogFormatted, result
 
-         if (search) {
+         matchLineSearch = /([\[][0-9]{2}:[0-9]{2}] .*?: \[.*?\] [^\[]+)/g
+         thisLogFormatted = fileContents.join('').match(new RegExp(matchLineSearch))
 
-            result = searchInFile(thisLogFormatted, searchOptions)
+         switch (mode) {
+            case 'langues' : {
 
-            if (result.datas.length > 0) {
-               searchResult.push({fileName: thisFile.name, datas: result.datas, allPos: result.numPos})
-               newFileListe.push(thisFile)
-            }
+               // matchLineSearch = /^([\[][0-9]{2}:[0-9]{2}])\s<color=.*?>\s(.*?\s)(\(.*?\)\s)(.*?)<\/color>$/gm
+               matchLineSearch = /^([\[][0-9]{2}:[0-9]{2}])\s<color=.*?>\s?(.*?\s):?(\(.*?\)\s)?(.*?)<\/color>$/gm
+               thisLogFormatted = fileContents.join('').match(new RegExp(matchLineSearch))
 
-            if (fileindex === listeFiles.length) {
+               for (let index in thisLogFormatted) {
+                  let line = thisLogFormatted[index]
+                  let newLine
+                  newLine = line.replace(/<color=.*?>\s?/g, '').replace(/<\/color>/g, '')
+                  // thisLogFormatted[index] = newLine
+                  // thisLogFormatted[index] = /([\[][0-9]{2}:[0-9]{2}])\s(.*?)\s(\(.*?\))\s(.*?)$/g.exec(newLine)
+                  thisLogFormatted[index] = /^([\[][0-9]{2}:[0-9]{2}])\s(.*?)(\(.*\)|:)\s(.*?)$$/g.exec(newLine)
+               }
 
-               if (searchResult.length > 0) {
-
-                  searchResult.sort((a, b) => {
-                     return a.fileName.localeCompare(b.fileName)
-                  })
-
-                  newFileListe.sort((a, b) => {
-                     return a.name.localeCompare(b.name)
-                  })
-
-                  searchResult.forEach(item => {
-                     allPos.push(item.allPos)
-                  })
-
-                  setupFileListing(newFileListe, true, allPos)
-
-                  document.getElementById('arrayRefSearch').value = JSON.stringify(searchResult)
-                  document.querySelector('.table_container').classList.add('resize')
-                  document.querySelector('#listing li.selected').click()
-
-               } else {
-                  const searchFail = document.querySelector('#searchFiles .search_fail')
-                  searchFail.innerHTML = `<span>La recherche du mot <strong>${searchOptions.termTosearch}</strong> n'a donnée aucun résultat</span>`
-                  searchFail.classList.add('show')
+               if (thisLogFormatted !== null) {
+                  languesLines.push({file: thisFile.name, lines: thisLogFormatted})
+               }
+               if (fileindex === listeFiles.length) {
+                  document.getElementById('languesListeRef').value = JSON.stringify(languesLines)
                   body.classList.remove('wait')
                }
+
             }
+               break
+            case 'search' : {
 
-         } else {
+               result = searchInFile(thisLogFormatted, searchOptions)
 
-            readFile(
-                thisLogFormatted,
-                thisFile.name,
-                searchOptions.termTosearch,
-                document.querySelector('.table_container').classList.contains('resize')
-            )
+               if (result.datas.length > 0) {
+                  searchResult.push({fileName: thisFile.name, datas: result.datas, allPos: result.numPos})
+                  newFileListe.push(thisFile)
+               }
 
-            tableResultListeners()
-            body.classList.remove('wait')
+               if (fileindex === listeFiles.length) {
+
+                  if (searchResult.length > 0) {
+
+                     searchResult.sort((a, b) => {
+                        return a.fileName.localeCompare(b.fileName)
+                     })
+
+                     newFileListe.sort((a, b) => {
+                        return a.name.localeCompare(b.name)
+                     })
+
+                     searchResult.forEach(item => {
+                        allPos.push(item.allPos)
+                     })
+
+                     setupFileListing(newFileListe, true, allPos)
+
+                     document.getElementById('arrayRefSearch').value = JSON.stringify(searchResult)
+                     document.querySelector('.table_container').classList.add('resize')
+                     document.querySelector('#listing li.selected').click()
+
+                  } else {
+                     const searchFail = document.querySelector('#searchFiles .search_fail')
+                     searchFail.innerHTML = `<span>La recherche du mot <strong>${searchOptions.termTosearch}</strong> n'a donnée aucun résultat</span>`
+                     searchFail.classList.add('show')
+                     body.classList.remove('wait')
+                  }
+               }
+            }
+               break
+            case 'process' : {
+
+               processLog(
+                   thisLogFormatted,
+                   thisFile.name,
+                   //searchOptions.termTosearch,
+                   document.querySelector('.table_container').classList.contains('resize')
+               )
+
+               tableResultListeners()
+               body.classList.remove('wait')
+            }
+               break
 
          }
+
          fileindex++
 
       })
@@ -671,7 +727,6 @@ const searchInFile = (thisLog, searchOptions) => {
                newMessage.push(message.replace(regEx, "<span class='highlight'>$&</span>"))
             }
 
-
          }
 
       } else {
@@ -698,7 +753,8 @@ const searchInFile = (thisLog, searchOptions) => {
 //    MISE EN FORME DU LOG
 // ---------------------------------------------------------
 
-const readFile = (thisFile, filename, word, resultByLine) => {
+// const processLog = (thisLog, filename, word, resultByLine) => {
+const processLog = (thisLog, filename, resultByLine) => {
 
    const fileContentTable = document.querySelector('#fileContent tbody')
    const resultByLineTable = document.querySelector('#resultByLine tbody')
@@ -707,23 +763,31 @@ const readFile = (thisFile, filename, word, resultByLine) => {
    let filePjName = (/^(.*)([_]|[ (])(Chatlog)/g).exec(filename)
    filePjName = filePjName[1].trim().replaceAll('_', ' ')
 
-   const listColors = lineColorByName(thisFile, filePjName)
+   // Assigne une couleur pour chaque pj/pnj
+   const listColors = lineColorByName(thisLog, filePjName)
+
    let formattedContent = []
    let formattedSearch = []
    let refResult, getRef, indexLine = 0
    let addRefLine
+   let formattedTraducByLine// = ''
 
+
+   let languageInThisLog = JSON.parse(document.getElementById('languesListeRef').value)
+   let getFileLangue = languageInThisLog.find(item => item.file === filename.replace('Chatlog', 'Combatlog'))
 
    if (resultByLine) {
       refResult = JSON.parse(document.getElementById('arrayRefSearch').value)
       getRef = refResult.find(item => item.fileName === filename)
    }
 
-   for (let line of thisFile) {
+   let prevHeure = ''
+
+   for (let thisLine of thisLog) {
 
       // regex : [00:00] [pseudo] (optionnel) Nom du PJ : [Type de message] -> jusqu'à la fin de l'entrée
       let search = /^([\[][0-9]{2}:[0-9]{2}])( \[.*?\])?(.*?: )(\[.*?\]) ([^\[]+)/g
-      let match = search.exec(line)
+      let match = search.exec(thisLine)
       let message = ''
 
       if (match !== null) {
@@ -734,19 +798,21 @@ const readFile = (thisFile, filename, word, resultByLine) => {
          let msgType = match[4]
          let msg = match[5].replaceAll(/(\r\n|\n|\r)/gm, "<br>")
 
-         // supprime les crochets + les espaces et passe tout en minuscule
+         // supprime les crochets + les espaces et passe tout en minuscule -> le type de message est utilisé en CSS.class
          let msgClass = msgType.slice(1, -1).replaceAll(' ', '').toLowerCase()
 
-         // Masque / Affiche les colonnes pseudos et type de message
+         // Masque / Affiche les colonnes pseudos
          const hideColPseudo = document.getElementById('colPseudo').checked
          let hideLinePseudo = hideColPseudo ? 'hide' : ''
 
+         // Masque / Affiche les colonnes type de message
          const hideColDialogType = document.getElementById('colDialogType').checked
          let hideLineMsgType = hideColDialogType ? 'hide' : ''
 
-         // Applique une couleur à chaque locuteur
+         // Applique une couleur à chaque pj/pnj (si "undefined" -> message serveur -> gris par défaut)
          // let lineColor = `hsl(${listColors.colors[listColors.names.indexOf(pjName)]}, 80%, 35%)`
-         let lineColor = listColors.colors[listColors.names.indexOf(pjName)] === undefined ? '#778999' : listColors.colors[listColors.names.indexOf(pjName)]
+         let indexPjName = listColors.names.indexOf(pjName)
+         let lineColor = listColors.colors[indexPjName] === undefined ? '#778999' : listColors.colors[indexPjName]
 
          // Recherche les émotes *...* et les mets en italique
          let emotes = msg.split(/([*].*?[*])/g)
@@ -763,16 +829,16 @@ const readFile = (thisFile, filename, word, resultByLine) => {
 
          // Tronque les noms de pj trop longs
          let fullName, btn, maxChar = 23
+         fullName = pjName
 
          if (pjName.length > maxChar) {
-            fullName = pjName
             pjName = pjName.slice(0, maxChar) + '...'
             btn = 'btn'
          }
 
+         // Met en surbrilance le mot recherché dans la colonne message, si rien, la ligne n'est pas modifiée
          message = msg
          let searchedLine = false
-
          if (resultByLine && getRef !== undefined) {
 
             addRefLine = getRef.datas.find(item => item.numLigne === indexLine)
@@ -781,6 +847,38 @@ const readFile = (thisFile, filename, word, resultByLine) => {
                searchedLine = true
                message = addRefLine.newMessage
             }
+
+         }
+
+         // Ajoute les traductions
+         let withTraduc = false
+         let getLangueLine
+         formattedTraducByLine = ''
+
+         if (getFileLangue !== undefined) {
+            getLangueLine = getFileLangue.lines.filter(line => line[1] === heure)
+            withTraduc = getLangueLine.length > 0
+         }
+
+         prevHeure = heure
+
+         if (prevHeure === heure) {
+
+            if (withTraduc) {
+
+               let thisItemLangue = getLangueLine[0]
+
+               if (thisItemLangue[2].trim() === fullName) {
+
+                  formattedTraducByLine = `<span class="traduc"><i>${thisItemLangue[3]}</i> ${thisItemLangue[4]}</span>`
+                  message = message + formattedTraducByLine
+
+                  if (getFileLangue !== undefined)
+                     getFileLangue.lines.shift()
+
+               }
+            }
+
          }
 
          // Ligne du tableau à ajouter
@@ -790,7 +888,7 @@ const readFile = (thisFile, filename, word, resultByLine) => {
                                  <td class="pseudo ${hideLinePseudo}" style="color: ${lineColor}">${pseudo}</td>
                                  <td class="pjName ${btn}" style="color: ${lineColor}" data-fullName = "${fullName}">${pjName}</td>
                                  <td class="${msgClass} msgType ${hideLineMsgType}" title="${msgClass}"><span>${msgType}</span></td>
-                                 <td class="${msgClass} message" style="color: ${lineColor}">${message}</td>
+                                 <td class="${msgClass} message" style="color: ${lineColor}">${message}</td>                                 
                               </tr>`
 
          formattedContent.push(formattedLine)
@@ -815,6 +913,10 @@ const readFile = (thisFile, filename, word, resultByLine) => {
       popupSearchFiles.querySelector('#goSearch').textContent = "Poursuivre la recherche"
       popupSearchFiles.querySelector('#goNewSearch').classList.remove('disabled')
    }
+
+   // if (getFileLangue !== null) {
+   //    document.querySelector('.traducByLine_container .traducByLine tbody').innerHTML = formattedTraduc.join('')
+   // }
 
    // Charge le array "formattedContent" dans le tableau <table/>
    fileContentTable.innerHTML = formattedContent.join('')
